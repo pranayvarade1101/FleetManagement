466644c28ddefc979087a78db8bdc06f23b9eeaa
Nathan <nathan13pereira2001@gmail.com>
2024-05-08 15:14:27 +0530

    Changed anchor tags to links

--- fm-client/src/admin-functions/ManageCustomer.js
+++ fm-client/src/admin-functions/ManageCustomer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./admin-functions.css"
 
 function ManageCustomer() {
--- fm-client/src/admin.js
+++ fm-client/src/admin.js
@@ -1,35 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import "./admin.css"
+
+function AdminButton({ to, children }) {
+  return (
+    <button className="btn">
+      <Link to={to}>{children}</Link>
+    </button>
+  );
+}
 
 function AdminPage() {
   return (
     <div className="admin-page">
-        <h1>Admin Dashboard</h1>
-            <div className="btn-container">
-                <button className="btn">
-                    <a href='/manage-customer'>
-                        Manage Customer
-                    </a>
-                </button>
-                <button className="btn">
-                    <a href='/manage-driver'>
-                        Manage Driver
-                    </a>
-                </button>
-                <button className="btn">
-                    <a href='/manage-vehicles'>
-                        Manage Vehicles
-                    </a>
-                </button>
-                <button className="btn">
-                    <a href='/feedbacks'>
-                        Feedbacks
-                    </a>
-                </button>
-            </div>
-        </div>
-    );
+      <h1>Admin Dashboard</h1>
+      <div className="btn-container">
+        <AdminButton to="/manage-customer">Manage Customer</AdminButton>
+        <AdminButton to="/manage-driver">Manage Drivers</AdminButton>
+        <AdminButton to="/manage-vehicles">Manage Vehicles</AdminButton>
+        <AdminButton to="/feedbacks">Feedback</AdminButton>
+      </div>
+    </div>
+  );
 }
 
 export default AdminPage;
