b9581869d6cd2d3396cec420927d6017ebf061e3
Nathan <nathan13pereira2001@gmail.com>
2024-05-15 15:38:11 +0530

    Added Routes to app.js

--- fm-client/src/App.jsx
+++ fm-client/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import NavBar from './myComponents/shared/navbar/Navbar';
+import PageHome from './myComponents/home/PageHome/PageHome';
+import BusHome from './myComponents/home/BusHome/BusHome';
+import AboutUs from './myComponents/about/AboutUs';
 import AdminPage from './myComponents/admin/admin';
 import ManageCustomer from './myComponents/admin/admin-functions/ManageCustomer';
 import ManageDriver from './myComponents/admin/admin-functions/ManageDriver';
@@ -12,14 +16,17 @@ import './App.css';
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
+        <Route path='/' element={<PageHome/>} />
+        <Route path='/home' element={<BusHome/>} />
+        <Route path='/about-us' element={<AboutUs/>} />
         <Route path='/admin' element={<AdminPage/>} />
         <Route path='/manage-customer' element={<ManageCustomer/>} />
         <Route path='/manage-driver' element={<ManageDriver/>} />
         <Route path='/manage-vehicles' element={<ManageVehicles/>} />
         <Route path='/feedbacks' element={<Feedbacks/>} />
       </Routes>
-
     </Router>
   );
 }
--- fm-client/src/myComponents/shared/navbar/Navbar.jsx
+++ fm-client/src/myComponents/shared/navbar/Navbar.jsx
@@ -1,52 +1,47 @@
 import React from 'react'
+import { Link, Outlet } from 'react-router-dom';
 import './navStyle.css'
 
 import 'bootstrap/dist/css/bootstrap.css';
-
-
 import Logo from "../../../assets/logo.svg";
-import { Link, Outlet } from 'react-router-dom';
 
 const Navbar = () => {
-
   const userAccountType='customer'; // for now hard-coded
-
   return (
-
     <div className='nav px-5 d-flex justify-content-between align-items-center '>
       <div className="nav-start d-flex align-items-center justify-content-center">
         <div className="nav-logo me-5 px-3">
           <img src={Logo} alt='logo' className='logo '/>
-          </div>
+        </div>
         FleetManagement 
       </div>
       <div className="nav-content d-flex justify-content-center align-items-center">
       {userAccountType === 'admin' && (
         <ul className="nav-menu d-flex align-items-end justify-content-center ">
-          <li> <a href="/home"> Home </a> </li>
-          <li> <a href="/admin"> Admin </a> </li>
-          <li> <a href="/about-us"> About Us </a> </li>
-          <li className='nav-login'> <a href="/login"> Login/SignUp </a> </li>
+          <li> <Link to="/">Home</Link> </li>
+          <li> <Link to="/admin">Admin</Link> </li>
+          <li> <Link to="/about-us">About Us</Link> </li>
+          <li className='nav-login'> <Link to="/login"> Login/SignUp </Link> </li>
 
         </ul>
       )}
       {userAccountType === 'customer' && (
         <ul className="nav-menu d-flex align-items-end justify-content-center ">
-          <li> <Link href="/home"> Home        </Link> </li>
-          <li> <Link href="/PageHome"> Explore </Link> </li>
-          <li> <Link href="/about-us"> About   </Link> </li>
-          <li> <Link href="/home"> Contact     </Link> </li>
-          <li className='nav-login'> <a href="/login"> Login/SignUp </a> </li>
+          <li> <Link to="/">Home</Link> </li>
+          <li> <Link to="/home">Explore</Link> </li>
+          <li> <Link to="/about-us">About</Link> </li>
+          <li> <Link to="/home">Contact</Link> </li>
+          <li className='nav-login'> <Link to="/login"> Login/SignUp </Link> </li>
           <Outlet></Outlet>
         </ul>
       )}
       {userAccountType === 'driver' && (
         <ul className="nav-menu d-flex align-items-end justify-content-center ">
-          <li> <a href="/home"> Home </a> </li>
-          <li><a href="/home"> Explore </a> </li>
-          <li> <a href="/about-us"> About </a> </li>
-          <li> <a href="/home"> Contact </a> </li>
-          <li className='nav-login'> <a href="/login"> Login/SignUp </a> </li>
+          <li> <Link to="/"> Home </Link> </li>
+          <li><Link to="/home"> Explore </Link> </li>
+          <li> <Link to="/about-us"> About </Link> </li>
+          <li> <Link to="/home"> Contact </Link> </li>
+          <li className='nav-login'> <Link to="/login"> Login/SignUp </Link> </li>
         </ul>
       )}
       </div>
@@ -54,4 +49,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
